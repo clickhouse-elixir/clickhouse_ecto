8da4c3a58f9adbfcb65a96f53e476b5b1baa624c
roman.chudov <roman.chudov@gmail.com>
2018-04-05 14:51:59 +0700

    Run clickhousex and clickhouse_ecto

--- examples/mix.exs
+++ examples/mix.exs
@@ -2,24 +2,30 @@ defmodule ExampleApp.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :example_app,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     elixirc_paths: elixirc_paths(Mix.env),
-     compilers: [:phoenix, :gettext] ++ Mix.compilers,
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     aliases: aliases(),
-     deps: deps()]
+    [
+      app: :example_app,
+      version: "0.0.1",
+      elixir: "~> 1.2",
+      elixirc_paths: elixirc_paths(Mix.env),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers,
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      aliases: aliases(),
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application.
   #
   # Type `mix help compile.app` for more information.
   def application do
-    [mod: {ExampleApp, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+    [
+      mod: {ExampleApp, []},
+      applications: [
+        :phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
+        :phoenix_ecto, :postgrex, :clickhousex, :clickhouse_ecto
+      ]
+    ]
   end
 
   # Specifies which paths to compile per environment.
@@ -30,15 +36,17 @@ defmodule ExampleApp.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.4"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"},
-     {:clickhouse_ecto, "~> 0.2.0"},]
+    [
+      {:phoenix, "~> 1.2.4"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_ecto, "~> 3.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:gettext, "~> 0.11"},
+      {:cowboy, "~> 1.0"},
+      {:clickhouse_ecto, "~> 0.2.0"}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
