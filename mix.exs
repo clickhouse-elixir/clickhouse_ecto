5387f394ade01828c4dfe8b299345eca063c29bd
santaux <santaux@gmail.com>
2018-07-27 12:06:05 +0300

    Increased version to 0.2.4 after subqueries fix

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ClickhouseEcto.Mixfile do
   def project do
     [
       app: :clickhouse_ecto,
-      version: "0.2.3",
+      version: "0.2.4",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
