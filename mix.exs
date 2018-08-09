2349a248926b78cbf4903a36cc33c612143f7f31
santaux <santaux@gmail.com>
2018-08-09 11:21:22 +0300

    Bumped version number to 0.2.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ClickhouseEcto.Mixfile do
   def project do
     [
       app: :clickhouse_ecto,
-      version: "0.2.4",
+      version: "0.2.5",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
