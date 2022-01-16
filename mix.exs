3400e346eebd54dd93db5c183ec7680e4b7591bd
Ivan Sokolov <ivan-p-sokolov@ya.ru>
2022-01-16 06:31:56 +0300

    Bump dependencies
    Bump clickhousex to 0.5.0, remove HTTPoison. Fix #25.

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule ClickhouseEcto.Mixfile do
   defp deps do
     [
       {:ecto_sql, "~> 3.0"},
-      {:clickhousex, "~> 0.4.0"},
+      {:clickhousex, "~> 0.5.0"},
       {:ex_doc, "~> 0.19", only: :dev},
       {:db_connection, "~> 2.2.1", override: true}
     ]
